e17ab13314a43d1872a15c640d86ec2ac332d740
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-08-14 15:42:35 +0200

    feat(hero): implement HeroComponent structure with SVG graphics and button integration

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../button/button";
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './hero.html',
   styleUrls: ['./hero.scss'],
 })
