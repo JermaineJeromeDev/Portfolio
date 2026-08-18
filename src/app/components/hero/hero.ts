75894a2e326402963b52b0fb8e9c91e3829b87fe
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-08-18 12:05:11 +0200

    feat(hero): adjust cyan blob positioning for improved layout on larger screens

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -80,8 +80,8 @@ export class HeroComponent implements AfterViewInit, OnDestroy {
       this.purpleX = textLeftInHero - 330;
       this.purpleY = textTopInHero - 260;
 
-      this.cyanX = Math.min(heroWidth - 100, textLeftInHero + textRect.width - 180);
-      this.cyanY = textTopInHero + 16;
+      this.cyanX = Math.min(heroWidth - 100, textLeftInHero + textRect.width - 140);
+      this.cyanY = textTopInHero -50;
     }
 
     else if (windowWidth >= 540) {
