7391822fd8abea839f3efef729176422beb136cb
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-08-22 20:49:35 +0200

    refactor(hero): add media query for ts-blob adjustments in landscape orientation

--- src/app/components/screen-rotate/screen-rotate.ts
+++ src/app/components/screen-rotate/screen-rotate.ts
@@ -2,10 +2,10 @@ import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-screen-rotate',
-  imports: [NgOptimizedImage],
-  templateUrl: './screen-rotate.html',
-  styleUrls: ['./screen-rotate.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-screen-rotate',
+    imports: [NgOptimizedImage],
+    templateUrl: './screen-rotate.html',
+    styleUrls: ['./screen-rotate.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ScreenRotateComponent {}
