0047623e1918523cd9f14ac2c6b59ecf9be57f3b
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-08-25 13:24:18 +0200

    feat(header): implement smooth scrolling to sections with offset on navigation links

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,7 +10,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withInMemoryScrolling({ anchorScrolling: 'enabled' })), 
+    provideRouter(routes, withInMemoryScrolling({ anchorScrolling: 'enabled' })),
     provideClientHydration(withEventReplay()),
     provideHttpClient(),
     provideTranslateService({
