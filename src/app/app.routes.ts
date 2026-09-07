42acc848bf77b46aa19ac5800a64edff6eb858f6
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-09-07 10:47:11 +0200

    feat(imprint, legal-notice): add imprint and legal notice components with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
+import { ImprintComponent } from './pages/imprint/imprint';
+import { LegalNoticeComponent } from './pages/legal-notice/legal-notice';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'legal-notice', component: LegalNoticeComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: '**', redirectTo: '' }
 ];
---
+++ src/app/pages/imprint/imprint.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imprint',
+  imports: [],
+  templateUrl: './imprint.html',
+  styleUrls: ['./imprint.scss'],
+})
+export class ImprintComponent {
+
+}
